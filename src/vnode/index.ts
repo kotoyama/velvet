5141a714c40304318a5e45f7d434f81ee56eec2b
Kate Maltseva <kotoyama@list.ru>
2020-12-06 18:06:17 +0500

    add virtual dom

---
+++ src/vnode/index.ts
@@ -0,0 +1,69 @@
+import { Children, MountedVNode, Props, Tag, VNode } from "./types";
+
+export interface NodeContainer {
+  appendChild(el: HTMLElement): void;
+}
+
+export function h(tag: Tag, props: Props | null, children: Children): VNode {
+  return VNode.valueOf(tag, props, children);
+}
+
+export function mount(vNode: VNode, container: NodeContainer): void {
+  const el = vNode.recreateDOM().el;
+  Object.entries(vNode.props).forEach(([name, value]) =>
+    el.setAttribute(name, value)
+  );
+  if (typeof vNode.children === "string") {
+    el.textContent = vNode.children;
+  } else {
+    vNode.children.forEach((child) => mount(child, el));
+  }
+  container.appendChild(el);
+}
+
+export function unmount(vNode: MountedVNode): void {
+  const parent = vNode.el.parentNode;
+  if (parent) {
+    parent.removeChild(vNode.el);
+  } else {
+    throw new Error(`Parent for ${JSON.stringify(vNode)} was not found`);
+  }
+}
+
+export function patch(n1: MountedVNode, n2: MountedVNode): void {
+  const el = (n2.el = n1.el);
+  if (!el.parentNode) {
+    throw new Error(`Parent of ${n1} was not found`);
+  }
+
+  if (n1.sameTagWith(n2)) {
+    mount(n2, el.parentNode);
+    unmount(n1);
+
+    return;
+  }
+
+  if (typeof n2.children === "string") {
+    el.textContent = n2.children;
+    return;
+  }
+
+  const c2 = n2.children;
+
+  if (typeof n1.children === "string") {
+    c2.forEach((c) => patch(n1, c));
+    return;
+  }
+  const c1 = n1.children;
+  const commonLength = Math.min(c1.length, c2.length);
+
+  for (let i = 0; i < commonLength; i++) {
+    patch(c1[i], c2[i]);
+  }
+  if (c2.length < c1.length) {
+    c1.slice(c2.length).forEach(unmount);
+  }
+  if (c1.length < c2.length) {
+    c2.slice(c1.length).forEach((c) => mount(c, el));
+  }
+}
---
+++ src/vnode/types.ts
@@ -0,0 +1,46 @@
+export class VNode {
+  static valueOf(tag: Tag, props: Props | null, children: Children): VNode {
+    return new VNode(tag, props || {}, children);
+  }
+
+  public _el: HTMLElement | null = null;
+
+  private constructor(
+    readonly tag: Tag,
+    readonly props: Props,
+    readonly children: Children
+  ) {}
+
+  get el(): HTMLElement {
+    return this._el || (this._el = this.createElement());
+  }
+
+  set el(el: HTMLElement) {
+    this._el = el;
+  }
+
+  recreateDOM(): this {
+    this._el = this.createElement();
+    return this;
+  }
+
+  sameTagWith(other: VNode): boolean {
+    return this.tag === other.tag;
+  }
+
+  private createElement(): HTMLElement {
+    return document.createElement(this.tag);
+  }
+}
+
+export interface MountedVNode extends VNode {
+  el: HTMLElement;
+  children: MountedChildren;
+}
+
+export type Tag = string;
+export type Props = {
+  [propName: string]: string;
+};
+export type Children = VNode[] | string;
+export type MountedChildren = MountedVNode[] | string;
